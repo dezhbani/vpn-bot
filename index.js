1845f15000ff5ae2b0deccbf76560ae24d7be352
matin dezhbani <dezhbani.matin@gmail.com>
2023-04-13 03:30:40 +0430

    disable axios

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: axios } = require('axios');
+// const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 const {Telegraf} = require('telegraf');
@@ -11,20 +11,20 @@ bot.hears('/start', ctx => {
 });
 let list = []
 bot.hears('/details', async ctx => {
-  list = (await axios.post('http://141.11.246.180:1000/xui/inbound/list',{}, {
-    withCredentials: true,
-    headers: {
-      'Cookie': 'session=MTY4MTEzNDU5NHxEdi1CQkFFQ180SUFBUkFCRUFBQVpmLUNBQUVHYzNSeWFXNW5EQXdBQ2t4UFIwbE9YMVZUUlZJWWVDMTFhUzlrWVhSaFltRnpaUzl0YjJSbGJDNVZjMlZ5XzRNREFRRUVWWE5sY2dIX2hBQUJBd0VDU1dRQkJBQUJDRlZ6WlhKdVlXMWxBUXdBQVFoUVlYTnpkMjl5WkFFTUFBQUFKZi1FSWdFQ0FRNXRZWFJwYmkxa1pYcG9ZbUZ1YVFFTlRVRlVTVTVrWlhwb1ltRnVhUUE9fD0t7BMKrYe1C5-70WxTY8SCsBY2p2kjvt90qlcwOyqw'
-    }
-  })).data.obj
+  // list = (await axios.post('http://141.11.246.180:1000/xui/inbound/list',{}, {
+  //   withCredentials: true,
+  //   headers: {
+  //     'Cookie': 'session=MTY4MTEzNDU5NHxEdi1CQkFFQ180SUFBUkFCRUFBQVpmLUNBQUVHYzNSeWFXNW5EQXdBQ2t4UFIwbE9YMVZUUlZJWWVDMTFhUzlrWVhSaFltRnpaUzl0YjJSbGJDNVZjMlZ5XzRNREFRRUVWWE5sY2dIX2hBQUJBd0VDU1dRQkJBQUJDRlZ6WlhKdVlXMWxBUXdBQVFoUVlYTnpkMjl5WkFFTUFBQUFKZi1FSWdFQ0FRNXRZWFJwYmkxa1pYcG9ZbUZ1YVFFTlRVRlVTVTVrWlhwb1ltRnVhUUE9fD0t7BMKrYe1C5-70WxTY8SCsBY2p2kjvt90qlcwOyqw'
+  //   }
+  // })).data.obj
   ctx.reply("enter config name")
-  list.map(config =>{
-    const download = (config.down / (1024 * 1024 * 1024)).toFixed(2) < 1? (config.down / (1024 * 1024)) : (config.down / (1024 * 1024 * 1024)).toFixed(2)
-    const downSymbol = (config.down / (1024 * 1024 * 1024)).toFixed(2) < 1 ? 'MB' : 'GB'
-    bot.hears(`${config.remark}`, ctx => {
-      ctx.reply(`config name: ${config.remark} \n downloaded: ${download} ${downSymbol} \n uploded: ${Math.floor(config.up / (1024 * 1024 ))} MB \n total: ${config.total / (1024 * 1024 * 1024)} GB`)
-    })
-  })
+  // list.map(config =>{
+  //   const download = (config.down / (1024 * 1024 * 1024)).toFixed(2) < 1? (config.down / (1024 * 1024)) : (config.down / (1024 * 1024 * 1024)).toFixed(2)
+  //   const downSymbol = (config.down / (1024 * 1024 * 1024)).toFixed(2) < 1 ? 'MB' : 'GB'
+  //   bot.hears(`${config.remark}`, ctx => {
+  //     ctx.reply(`config name: ${config.remark} \n downloaded: ${download} ${downSymbol} \n uploded: ${Math.floor(config.up / (1024 * 1024 ))} MB \n total: ${config.total / (1024 * 1024 * 1024)} GB`)
+  //   })
+  // })
 // console.log(list);
 });
 
