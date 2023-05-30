02e36ae6c42b3378cd6bba44d50817f28aefa011
matin dezhbani <dezhbani.matin@gmail.com>
2023-05-30 11:45:57 +0330

    auth route for athentication users

---
+++ app/router/user/auth.js
@@ -0,0 +1,10 @@
+const { userAuthControllers } = require("../../controllers/user/auth/auth.controller");
+
+const router = require("express").Router();
+
+router.post("/get-otp", userAuthControllers.getOTP);
+router.post("/check-otp", userAuthControllers.checkOTP);
+
+module.exports = {
+    userAuthRoutes: router
+}
