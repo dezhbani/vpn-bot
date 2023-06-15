1072ea9939d6abce26249d99353220f8d1fb6d64
matin dezhbani <dezhbanimatin@gmail.com>
2023-06-15 16:01:23 +0330

    vallidation mobile function & add get time to "timestampToDate" function

--- bot/utils/functions.js
+++ bot/utils/functions.js
@@ -9,21 +9,33 @@ const totalConsumed = (up, down) => {
     const total = up + down
     return clculate(total)
 }
-const timestampToDate = (timestamp) =>{
+const timestampToDate = (timestamp, time=false) =>{
     const date = new Date(timestamp);
     if(date.getFullYear() < new Date().getFullYear()){
       return "بدون محدودیت زمانی"
     }else{
-        const year = date.getFullYear()
-        const month = date.getMonth() + 1
-        const day = date.getDate() 
-        console.log(year, month, day);
-      return farvardin.gregorianToSolar(year , month , day , "string")
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate() ;
+        const fullDate = farvardin.gregorianToSolar(year , month , day , "string");
+        if(time){
+            const hours = date.getHours();
+            const minute = date.getMinutes();
+            const second = date.getSeconds();
+            return `${fullDate} ${hours}:${minute}:${second}`
+        }
+      return fullDate
     }
+}
+const validationMobile = mobile => {
+    const mobileRegex = /^09[0-9]{9}$/
+    const isMobile = mobileRegex.test(mobile)
+    return !!isMobile
 }
 
 module.exports = {
     clculate,
     totalConsumed,
-    timestampToDate
+    timestampToDate,
+    validationMobile
 }
