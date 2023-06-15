cc5577a35de45cc6d2413ef1a1a9470e98662a96
matin dezhbani <dezhbanimatin@gmail.com>
2023-06-15 15:57:58 +0330

    add bills and account section to start

--- bot/commands/start.js
+++ bot/commands/start.js
@@ -1,17 +1,21 @@
 const {Telegraf, Markup} = require('telegraf');
 const { details } = require('../src/details');
 const { plans } = require('../src/plans');
+const { account } = require('../src/account');
+const { bill } = require('../src/bill');
 const { BOT_TOKEN } = process.env;
 
 const startTelegramBot = () =>{
     const bot = new Telegraf(BOT_TOKEN);
         bot.start(ctx => {
         const message = ctx.update.message
-        ctx.reply(`سلام ${message.chat.first_name} خوش اومدی!🤗`, Markup.keyboard([['جزییات کانفیگ'], ['پلن ها']])
+        ctx.reply(`سلام ${message.chat.first_name} خوش اومدی!🤗`, Markup.keyboard([["🖥 حساب کاربری"], ['🛍 پلن ها']])
             .oneTime()
             .resize()
             );
         });
+        bill(bot)
+        account(bot)
         details(bot)
         plans(bot)
         bot.launch();
