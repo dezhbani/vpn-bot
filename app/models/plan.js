e1e1ece5a2ae69739a3e844f75cc20a1eda76618
matin dezhbani <dezhbanimatin@gmail.com>
2023-06-25 09:51:25 +0330

    delete count field from model & and change all plan field to required in validation

--- app/models/plan.js
+++ app/models/plan.js
@@ -6,7 +6,6 @@ const planSchema = new mongoose.Schema({
     user_count: {type: String, required: true},
     data_size: {type: String, required: true},
     pay_link: {type: String, required: false},
-    count: {type: String, default: "0"},
     month: {type: Number, default: 1}
 }, {
     versionKey: false
--- app/validations/admin/plan.schema.js
+++ app/validations/admin/plan.schema.js
@@ -2,13 +2,12 @@ const Joi = require("@hapi/joi");
 const createHttpError = require("http-errors");
 
 const addPlanSchema = Joi.object({
-    name: Joi.string().min(1).max(40).error(createHttpError.BadRequest("عنوان پلن صحیح نمی باشد")),
-    price: Joi.number().error(createHttpError.NotFound("قیمت مورد نظر  صحیح نمی باشد")),
-    user_count: Joi.number().error(createHttpError.NotFound("تعداد کاربر مورد نظر  صحیح نمی باشد")),
-    data_size: Joi.number().error(createHttpError.NotFound("حجم بسته مورد نظر صحیح نمی باشد")),
-    pay_link: Joi.string().error(createHttpError.NotFound("لینک درگاه پرداخت  صحیح نمی باشد")),
-    count: Joi.number().error(createHttpError.NotFound("تعداد مورد صحیح نمی باشد")),
-    month: Joi.number().error(createHttpError.NotFound("ماه مورد صحیح نمی باشد")),
+    name: Joi.string().required().min(1).max(40).error(createHttpError.BadRequest("عنوان پلن صحیح نمی باشد")),
+    price: Joi.number().required().error(createHttpError.NotFound("قیمت مورد نظر  صحیح نمی باشد")),
+    user_count: Joi.number().required().error(createHttpError.NotFound("تعداد کاربر مورد نظر  صحیح نمی باشد")),
+    data_size: Joi.number().required().error(createHttpError.NotFound("حجم بسته مورد نظر صحیح نمی باشد")),
+    pay_link: Joi.string().required().error(createHttpError.NotFound("لینک درگاه پرداخت  صحیح نمی باشد")),
+    month: Joi.number().required().error(createHttpError.NotFound("ماه مورد صحیح نمی باشد")),
 })
 
 module.exports = {
