9edea775bf2e5615b4629546ce9679a7ee557eb6
matin dezhbani <dezhbanimatin@gmail.com>
2023-06-26 22:42:59 +0330

    add user section api

---
+++ src/components/admin/services/users.service.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+const getUsers = async() => {
+    const result = await axios.get('admin/user')
+    return await result.data.users
+}
+const getUserDetails = async(id) => {
+    const result = await axios.get(`admin/user/${id}`)
+    return await result.data.user
+}
+const addUser = async data => {
+    const { name, price, user_count, data_size, pay_link, count, month } = data
+    const result = await axios.post('admin/user/add', {
+        name,
+        price, 
+        user_count, 
+        data_size, 
+        pay_link, 
+        count,
+        month
+    })
+    return result.data
+}
+const repurchase = async id => {
+    const result = await axios.post(`admin/config/repurchase/${id}`)
+    return result.data
+}
+
+export {
+    getUsers,
+    addUser,
+    getUserDetails,
+    repurchase
+}
