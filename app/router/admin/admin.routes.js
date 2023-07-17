678341b260b7c77592d8cb2313a9946cc0219fb0
matin dezhbani <dezhbanimatin@gmail.com>
2023-07-17 17:33:27 +0330

    add check permissin middleware to router

--- app/router/admin/admin.routes.js
+++ app/router/admin/admin.routes.js
@@ -1,11 +1,15 @@
 const { adminConfigRoutes } = require("./config");
+const { adminPermissionRoutes } = require("./permission");
 const { adminPlanRoutes } = require("./plan");
+const { adminRoleRoutes } = require("./role");
 const { adminUserRoutes } = require("./user");
 const router = require("express").Router();
 
 router.use("/plan", adminPlanRoutes);
 router.use("/config", adminConfigRoutes);
 router.use("/user", adminUserRoutes);
+router.use("/permission", adminPermissionRoutes);
+router.use("/role", adminRoleRoutes);
 
 module.exports = {
     AdminRoutes: router
--- app/router/router.js
+++ app/router/router.js
@@ -1,10 +1,12 @@
+const { checkPermission } = require("../middlewares/permission.guard");
+const { verifyToken } = require("../middlewares/verifyAccessToken");
 const { AdminRoutes } = require("./admin/admin.routes");
 const { userAuthRoutes } = require("./user/auth");
 
 const router = require("express").Router(); 
 
 router.use("/auth", userAuthRoutes);
-router.use("/admin", AdminRoutes);
+router.use("/admin", verifyToken, checkPermission(), AdminRoutes);
 
 module.exports = {
     AllRoutes: router
