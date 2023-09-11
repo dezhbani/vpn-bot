d7abbf8ac9f5fb14323562780eca20624aef7fea
matin dezhbani <dezhbanimatin@gmail.com>
2023-09-11 14:46:06 +0330

    change type to number

--- app/models/plan.js
+++ app/models/plan.js
@@ -2,9 +2,9 @@ const { default: mongoose } = require("mongoose");
 
 const planSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    price: {type: String, required: true},
-    user_count: {type: String, required: true},
-    data_size: {type: String, required: true},
+    price: {type: Number, required: true},
+    user_count: {type: Number, required: true},
+    data_size: {type: Number, required: true},
     pay_link: {type: String, required: false},
     month: {type: Number, default: 1}
 }, {
