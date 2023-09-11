974b221536688ab18517050f7edfce2ed015b717
matin dezhbani <dezhbanimatin@gmail.com>
2023-09-11 14:22:52 +0330

    add profile section admin route

--- app/router/admin/admin.routes.js
+++ app/router/admin/admin.routes.js
@@ -1,6 +1,7 @@
 const { adminConfigRoutes } = require("./config");
 const { adminPermissionRoutes } = require("./permission");
 const { adminPlanRoutes } = require("./plan");
+const { adminProfileRoutes } = require("./profile");
 const { adminRoleRoutes } = require("./role");
 const { adminUserRoutes } = require("./user");
 const router = require("express").Router();
@@ -10,6 +11,7 @@ router.use("/config", adminConfigRoutes);
 router.use("/user", adminUserRoutes);
 router.use("/permission", adminPermissionRoutes);
 router.use("/role", adminRoleRoutes);
+router.use("/profile", adminProfileRoutes);
 
 module.exports = {
     AdminRoutes: router
