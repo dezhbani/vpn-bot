f2b43be244d0f9bb5e1e9f7913ae5f417be6b83e
matin dezhbani <dezhbanimatin@gmail.com>
2023-09-11 17:32:15 +0330

    add wallet & owner(by) & role and ... field

--- app/models/user.js
+++ app/models/user.js
@@ -6,12 +6,16 @@ const ConfigSchema = new mongoose.Schema({
     configID: {type: String, required: true}
 })
 const bill = new mongoose.Schema({
-    planID: {type: mongoose.Types.ObjectId, ref: 'plan', required: true},
-    buy_date: {type: Number, default: new Date().getTime()}
+    planID: {type: mongoose.Types.ObjectId, ref: 'plan', default: null},
+    buy_date: {type: Number, default: new Date().getTime()},
+    for: {type: String, default: '', required: true}, 
+    price: {type: Number, default: null},
+    up: {type: Boolean, default: true}
 })
 const UserSchema = new mongoose.Schema({
     first_name: {type: String},
     last_name: {type: String},
+    full_name: {type: String, default: ''},
     mobile: {type: String},
     bills: {type: [bill], ref: 'plan', default: []},
     configs: {type: [ConfigSchema], default: []},
@@ -20,6 +24,9 @@ const UserSchema = new mongoose.Schema({
         expireIn: 0
     }},
     chatID: {type: String},
+    wallet: {type: Number, default: 0},
+    by: {type: mongoose.Types.ObjectId, ref: 'users', default: this._id},
+    role: {type: String, default: 'customer'}
 }, {
     versionKey: false
 })
