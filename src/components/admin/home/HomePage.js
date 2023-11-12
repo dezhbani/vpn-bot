99708fb84817c7d9d08ddff47e5046c24a1a09d5
matin dezhbani <dezhbanimatin@gmail.com>
2023-11-12 21:35:23 +0330

    add new assets

--- src/components/admin/home/HomePage.js
+++ src/components/admin/home/HomePage.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Sidebar from '../public/Sidebar';
+// import { useHistory } from 'react-router-dom';
+import { ProfileContext } from '../../context/UserProfileContext'
+import Loading from '../public/Loading';
+import { getUserProfile } from '../../services/profile.service';
 
 const HomePage = () => {
+    // const [profile, setProfile] = useState()
+    // const profile = useContext(ProfileContext)
+    // const userProfile = async () => {
+    //     const sss= await getUserProfile(setProfile)
+    //     // if(profile.role !== "user") redirect('/sign-up')
+    // }
+    // userProfile()
     return (
         <div>
-            <Sidebar />
-            <h1>home</h1>
+            <Sidebar>
+                <h1>home</h1>
+            </Sidebar>
         </div>
     );
 };
