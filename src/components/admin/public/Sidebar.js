a7217bd85fe191ab29999e6f27f02410b53d414f
matin dezhbani <dezhbanimatin@gmail.com>
2023-11-12 21:19:45 +0330

    create Loading page & set to center of pages

--- src/components/admin/public/Sidebar.js
+++ src/components/admin/public/Sidebar.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import profile from '../assets/profile.png'
+import React, { useContext } from 'react';
 import plans from '../assets/plans.svg'
 import users from '../assets/user.svg'
+import userProfile from '../assets/profile.png'
 import configs from '../assets/configs.svg'
 import payments from '../assets/payment.svg'
 import home from '../assets/home.svg'
 //Styles
 import styles from './Sidebar.module.css'
 import { Link } from 'react-router-dom';
+import { ProfileContext } from '../../context/UserProfileContext';
+import Loading from './Loading';
 
 const Sidebar = () => {
+    const profile = useContext(ProfileContext)
+    if(!profile?.role) return <div className={styles.Loading}><Loading /></div>
+
     return (
         <div className={styles.main}>
             <div className={styles.profile}>
                 <div className={styles.imageContainer}>
-                    <img className={styles.profileImage} alt='profile' src={profile} />
+                    <img className={styles.profileImage} alt='profile' src={userProfile} />
                     <div className={styles.addImage}/>
                 </div>
-                <div className={styles.fullname}>matin dezhbani</div>
+                <div className={styles.fullname}>{profile.full_name}</div>
             </div>
             <div className={styles.fieldsContainer}>
                 <Link className={styles.links} to='/dashboard'>
@@ -44,7 +49,7 @@ const Sidebar = () => {
                         <div className={styles.field}>کانفیگ ها</div>
                     </div>
                 </Link>
-                <Link className={styles.links} to='/dashboard/payments'>
+                <Link className={styles.links} to='/dashboard/bills'>
                     <div className={styles.container}>
                         <img className={styles.icon} src={payments} alt='payments'/>
                         <div className={styles.field}>تراکنش ها</div>
@@ -52,7 +57,8 @@ const Sidebar = () => {
                 </Link>
             </div>
         </div>
-    );
+    )
+    
 };
 
 export default Sidebar;
