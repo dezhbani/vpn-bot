1ad2546b3d5c562fbf71113c9d5c6e9b31ed3144
matin dezhbani <dezhbanimatin@gmail.com>
2023-12-27 23:35:32 +0330

    Modal component to create custome modal

---
+++ src/components/public/components/Modal.js
@@ -0,0 +1,29 @@
+// Modal.js
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Modal.css'
+
+const Modal = ({ isOpen, onClose, children }) => {
+
+  return (
+    isOpen && (
+    <div className="modal-overlay">
+        <div onClick={onClose} className='bg-black opacity-50 h-full w-full fixed'></div>
+        <div className='modal-container'>
+            <div className="modal">
+                <div className="modal-content">{children}</div>
+            </div>
+        </div>
+    </div>
+    )
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
