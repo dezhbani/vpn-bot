bcb8b28e6eb03d1a35223e700636ace23822f2b6
matin dezhbani <dezhbanimatin@gmail.com>
2023-12-28 08:16:44 +0330

    add update wallet button & show user wallet credit

--- src/components/admin/public/Sidebar.js
+++ src/components/admin/public/Sidebar.js
@@ -1,61 +1,76 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import plans from '../assets/plans.svg'
 import users from '../assets/user.svg'
 import userProfile from '../assets/profile.png'
 import configs from '../assets/configs.svg'
 import payments from '../assets/payment.svg'
 import home from '../assets/home.svg'
+import wallet from '../assets/Wallet.svg'
 //Styles
 import styles from './Sidebar.module.css'
 import { Link } from 'react-router-dom';
 import { ProfileContext } from '../../context/UserProfileContext';
 import Loading from './Loading';
+import { addCommaToPrice } from '../../public/function';
+import UpdateWallet from '../payment-section/UpdateWallet';
 
 const Sidebar = () => {
+    const [ open, setOpen ] = useState(false)
+    const handleOpen = () => setOpen(true);
     const profile = useContext(ProfileContext)
     if(!profile?.role) return <div className={styles.Loading}><Loading /></div>
-
     return (
         <div className={styles.main}>
+            <UpdateWallet setOpen={setOpen} open={open} userID={profile._id} />
+            <div onClick={handleOpen} className={styles.wallet}>
+                <span>ت {addCommaToPrice(profile.wallet)}</span>
+                <img className='h-10' src={wallet} />
+            </div>
             <div className={styles.profile}>
                 <div className={styles.imageContainer}>
                     <img className={styles.profileImage} alt='profile' src={userProfile} />
-                    <div className={styles.addImage}/>
+                    {/* <div className={styles.addImage}/> */}
                 </div>
                 <div className={styles.fullname}>{profile.full_name}</div>
             </div>
-            <div className={styles.fieldsContainer}>
+            <ul className={styles.fieldsContainer}>
                 <Link className={styles.links} to='/dashboard'>
-                    <div className={styles.container}>
+                    <li className={styles.container}>
                         <img className={styles.icon} src={home} alt='home'/>
                         <div className={styles.field}>خانه</div>
-                    </div>
+                    </li>
                 </Link>
                 <Link className={styles.links} to='/dashboard/users'>
-                    <div className={styles.container}>
+                    <li className={styles.container}>
                         <img className={styles.icon} src={users} alt='users'/>
                         <div className={styles.field}>کاربران</div>
-                    </div>
+                    </li>
                 </Link>
                 <Link className={styles.links} to='/dashboard/plans'>
-                    <div className={styles.container}>
+                    <li className={styles.container}>
                         <img className={styles.icon} src={plans} alt='plans'/>
                         <div className={styles.field}>پلن ها</div>
-                    </div>
+                    </li>
                 </Link>
                 <Link className={styles.links} to='/dashboard/configs'>
-                    <div className={styles.container}>
+                    <li className={styles.container}>
                         <img className={styles.icon} src={configs} alt='configs'/>
                         <div className={styles.field}>کانفیگ ها</div>
-                    </div>
+                    </li>
                 </Link>
                 <Link className={styles.links} to='/dashboard/bills'>
-                    <div className={styles.container}>
+                    <li className={styles.container}>
                         <img className={styles.icon} src={payments} alt='payments'/>
                         <div className={styles.field}>تراکنش ها</div>
-                    </div>
+                    </li>
                 </Link>
-            </div>
+                <Link className={styles.links} to='/dashboard/support'>
+                    <li className={styles.container}>
+                        <img className={styles.icon} src={payments} alt='payments'/>
+                        <div className={styles.field}>پشتیبانی ها</div>
+                    </li>
+                </Link>
+            </ul>
         </div>
     )
     
