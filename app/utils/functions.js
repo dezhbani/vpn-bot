db44228d11f6ca9a5eae8967b7a4cb2d51aaaeed
matin dezhbani <dezhbanimatin@gmail.com>
2024-01-11 20:06:55 +0330

    create "create config" function and other function for this

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -2,7 +2,12 @@ const { userModel } = require("../models/user");
 const JWT = require("jsonwebtoken");
 const createError = require("http-errors");
 const createHttpError = require("http-errors");
-const { ACCESS_TOKEN_SECRET_KEY } = process.env;
+const jMoment = require("moment-jalali");
+const { createVlessKcp } = require("./config.type");
+const { default: axios } = require("axios");
+const { planModel } = require("../models/plan");
+const { V2RAY_API_URL, V2RAY_PASSWORD, V2RAY_USERNAME, ACCESS_TOKEN_SECRET_KEY } = process.env
+
 function randomNumber(){
     return Math.floor((Math.random() * 90000) + 10000)
 }
@@ -24,10 +29,10 @@ async function signAccessToken(userID){
 function copyObject(object){
     return JSON.parse(JSON.stringify(object))
 }
-function configExpiryTime(month){
+function configExpiryTime(month) {
     const expiryTime = new Date()
     expiryTime.setMonth(expiryTime.getMonth() + month)
-    expiryTime.setDate(expiryTime.getDate())
+    expiryTime.setDate(expiryTime.getDate()) 
     expiryTime.setHours(23)
     expiryTime.setMinutes(59)
     expiryTime.setSeconds(0)
@@ -35,7 +40,7 @@ function configExpiryTime(month){
     return expiryTime.getTime()
 }
 
-function randomString(){
+const randomString = () => {
     const random = Math.random().toString(36).substring(2, 8);
     return random
 }
@@ -62,6 +67,70 @@ const percentOfNumber = (number, percent) => {
     const result = (+number / 100) * percent;
     return result
 }
+const invoiceNumberGenerator = () => {
+    return jMoment().format("jYYYYjMMjDDHHmmssSSS") + String(process.hrtime()[1]).padStart(9, 0)
+}
+const tomanToRial = (rial) => {
+    return rial * 10
+}
+const rialToToman = (toman) => {
+    return toman / 10
+}
+const getConfigID = async () =>{
+    const configs = ((await axios.post(`${V2RAY_API_URL}/xui/inbound/list`,{}, {
+        withCredentials: true,
+        headers: {
+          'Cookie': cookie
+        }
+    })).data.obj)
+    const lastIndex = configs.length - 1
+    const lastConfigID = configs[lastIndex].id + 1
+    return lastConfigID
+}
+const createConfig = async (mobile, planID) => {
+        const plan = await findPlanByID(planID);
+        const lastConfigID = await getConfigID();
+        const user = await userModel.findOne({mobile})
+        const fullName = `${user.first_name} ${user.last_name}`;
+        const { details, configContent: config_content, id } = await createVlessKcp(lastConfigID, plan, fullName)
+        console.log(axios.defaults.jar);
+        const addConfig = await axios.post(`${V2RAY_API_URL}/xui/inbound/add`, details, {
+            withCredentials: true,
+            headers: {
+                'Cookie': cookie
+            }
+        })
+        const configs = {
+            name: fullName,
+            config_content,
+            expiry_date: +configExpiryTime(plan.month), 
+            configID: id
+        }
+        const bills = {
+            planID,
+            buy_date: new Date().getTime(),
+            for: {
+                description: 'خرید کانفیگ'
+            },
+            up: true, 
+            price: plan.price
+        }
+        if(!addConfig.data.success) return createHttpError.InternalServerError("کانفیگ ایجاد نشد")
+        const obj = {configs, bills}
+        return obj
+}
+const findPlanByID = async (planID, type) => {
+    const plan = await planModel.findById(planID);
+    if (!plan) throw createHttpError.NotFound("پلنی یافت نشد");
+    return plan
+}
+const getV2rayCookie = async () => {
+    const loginResponse = await axios.post('http://s1.delta-dev.top:1000/login', {
+        username: V2RAY_USERNAME,
+        password: V2RAY_PASSWORD
+    });
+    process.env.V2RAY_TOKEN = loginResponse.headers['set-cookie'][0].split(';')[0];
+}
 module.exports = {
     randomNumber,
     signAccessToken,
@@ -71,5 +140,10 @@ module.exports = {
     copyObject,
     deleteInvalidProperties,
     lastIndex,
-    percentOfNumber
+    percentOfNumber,
+    invoiceNumberGenerator,
+    rialToToman,
+    tomanToRial,
+    createConfig,
+    getV2rayCookie
 }
