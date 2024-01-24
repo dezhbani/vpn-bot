901a26308c51963f0e669c693c0f864ac7345855
matin dezhbani <dezhbanimatin@gmail.com>
2024-01-24 23:24:18 +0330

    update 'check end time of configs' function

--- app/controllers/admin/cron/checkConfigEndTime.js
+++ app/controllers/admin/cron/checkConfigEndTime.js
@@ -1,5 +1,4 @@
 const { bot } = require("../../../../bot/commands/start");
-const { timestampToDate } = require("../../../../bot/utils/functions");
 const { userModel } = require("../../../models/user");
 const { smsService } = require("../../../services/sms.service");
 
@@ -7,19 +6,20 @@ const checkEndTime = async (alertDay = 2) => {
     const users = await userModel.find();
     users.filter(user => {
         user.configs.map( async config => {
+            // user config time
             const time = new Date(+config.expiry_date);
-            const day = time.getDate()
             const month = time.getMonth() 
             const year = time.getFullYear()
-            const nowDay = new Date().getDate()
+            // // now time
+            const today = new Date();
             const nowMonth = new Date().getMonth() 
             const nowYear = new Date().getFullYear()
-            time.setDate(nowDay + 2)
             time.setMonth(nowMonth + 1)
-            if(day - alertDay == nowDay && month == nowMonth && year == nowYear ) {
-                console.log('time:',`${user.first_name} ${user.last_name}`);
-                // smsService.endTimeMessage(user.mobile, user.full_name, alertDay)
-                // if(user.chatID) bot.telegram.sendMessage(user.chatID, `${user.first_name} ${user.last_name} محترم 2 روز تا پایان زمان کانفیگتون باقی مانده`)
+            const daysUntilTime = Math.ceil((time - today) / (1000 * 60 * 60 * 24));
+            if(daysUntilTime == alertDay && month == 1 && year == nowYear && config.status ) {
+                const { mobile, first_name, last_name, full_name, chatID } = user;
+                // smsService.endTimeMessage(mobile, full_name, alertDay)
+                // if(user.chatID) bot.telegram.sendMessage(chatID, `${first_name} ${last_name} محترم 2 روز تا پایان زمان کانفیگتون باقی مانده`)
             }
         })
     })
