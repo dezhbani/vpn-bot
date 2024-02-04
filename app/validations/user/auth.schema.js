f521852a119864c59f881354b5c2de573128f0a1
matin dezhbani <dezhbanimatin@gmail.com>
2024-02-04 10:47:58 +0330

    fix error message

--- app/validations/user/auth.schema.js
+++ app/validations/user/auth.schema.js
@@ -1,10 +1,11 @@
 const Joi = require("@hapi/joi");
+const createHttpError = require("http-errors");
 const getOtpSchema = Joi.object({
-    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error("شماره موبایل وارد شده صحیح نمی باشد"))
+    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(createHttpError.BadRequest("شماره موبایل وارد شده صحیح نمی باشد"))
 });
 const checkOtpSchema = Joi.object({
-    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error("شماره موبایل صحیح نمی باشد")),
-    code : Joi.string().min(4).max(6).error(new Error("کد ارسال شده صحیح نمی باشد")),
+    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(createHttpError.BadRequest("شماره موبایل صحیح نمی باشد")),
+    code : Joi.string().min(4).max(6).error(createHttpError.BadRequest("کد ارسال شده صحیح نمی باشد")),
 });
 
 module.exports = {
