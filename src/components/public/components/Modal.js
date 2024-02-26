e16cdbd731681cfc7ecb29927631ee4a3c18645a
matin dezhbani <dezhbanimatin@gmail.com>
2024-02-26 14:40:58 +0330

    update Modal component to show loading

--- src/components/public/components/Modal.js
+++ src/components/public/components/Modal.js
@@ -3,21 +3,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Modal.css'
+import Loading from '../../admin/public/Loading';
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen , onClose, children, loading }) => {
 
   document.body.style.overflow = isOpen? 'hidden' : ''
 
   return (
-    isOpen && (
-    <div className="modal-overlay">
-        <div onClick={onClose} className='bg-black opacity-50 h-full w-full fixed'></div>
+    isOpen && loading? (
+      <div className="modal-overlay">
+        <div className='bg-white opacity-50 h-full w-full fixed'></div>
         <div className='modal-container'>
-            <div className="modal">
-                <div className="modal-content">{children}</div>
-            </div>
+                <Loading />
         </div>
-    </div>
+      </div>
+    ): isOpen && (
+      <div className="modal-overlay">
+          <div onClick={onClose} className='bg-black opacity-50 h-full w-full fixed'></div>
+          <div className='modal-container'>
+              <div className="modal">
+                  <div className="modal-content">{children}</div>
+              </div>
+          </div>
+      </div>
     )
   );
 };
@@ -25,6 +33,7 @@ const Modal = ({ isOpen, onClose, children }) => {
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
   children: PropTypes.node.isRequired,
 };
 
