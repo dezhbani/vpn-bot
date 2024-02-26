2be6edf3421996a583a7bd597a62db6be7de65a3
matin dezhbani <dezhbanimatin@gmail.com>
2024-02-26 13:47:24 +0330

    replacing custome Modal component with mui Modal

--- src/components/admin/payment-section/UpdateWallet.js
+++ src/components/admin/payment-section/UpdateWallet.js
@@ -1,54 +1,41 @@
-import { Box, Button, Modal, TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import styles from '../user-section/AddConfig.module.css'
 import { addConfig } from '../../services/config.service';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { headers } from '../../public/function';
+import Modal from '../../public/components/Modal';
 
 const UpdateWallet = ({setOpen, open, userID}) => {
     const [data, setData] = useState({})
+    const [loading, setLoading] = useState(false)
     const handleClose = () => setOpen(false);
     const change = event =>{
         setData({...data, [event.target.name]: event.target.value});
     }
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 300,
-        bgcolor: 'background.paper',
-        borderRadius: '10px',
-        boxShadow: 24,
-        p: 4,
-    };
     const sendData = async () =>{
+        setLoading(true)
         const result = await axios.patch(`admin/user/wallet/${userID}`, {
             pay: data.wallet
         }, headers).catch(err => {
-            console.log(err);
+            handleClose()
+            setLoading(false)
             toast.error(err.response?.data?.message)
         })
-        console.log(result);
-        if (result) {
-            
-        }
-        document.location.href = result.data?.payLink
+        if (result?.data) {
+            document.location.replace(result.data?.payLink)
             handleClose()
+        }
     }
     return (
-        <div className={styles.box}>
-            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-                <Box sx={style}>
-                    <h2>شارژ حساب کاربری</h2>
-                    <div className={styles.inputBox}>
-                        <TextField margin='normal' size='small' onChange={change} name='wallet' value={data.wallet} className={styles.field} label='(تومان)مبلغ' variant="outlined" />
-                        <Button onClick={sendData}>پرداخت</Button>
-                    </div>
-                </Box>
-            </Modal>
-        </div>
+        <Modal isOpen={open} onClose={handleClose} loading={loading}>
+            <h2>شارژ حساب کاربری</h2>
+            <div className={styles.inputBox}>
+                <TextField margin='normal' size='small' onChange={change} name='wallet' value={data.wallet} className={styles.field} label='(تومان)مبلغ' variant="outlined" />
+                <Button onClick={sendData}>پرداخت</Button>
+            </div>
+        </Modal>
     );
 };
 
