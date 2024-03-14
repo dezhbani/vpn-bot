7ba3588d91d1397b6395405c75812a145e082c92
matin dezhbani <dezhbanimatin@gmail.com>
2024-03-14 12:33:03 +0330

    create no config component for empty configs array

--- src/components/admin/config-section/Configs.js
+++ src/components/admin/config-section/Configs.js
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from '../public/Sidebar';
 import styles from './Configs.module.css';
-import { changeConfigsStatus, getConfigsList } from '../../services/config.service';
+import { changeConfigsStatus, getConfigsByDay, getConfigsList } from '../../services/config.service';
 import Config from './Config';
 import Modal from '../../public/components/Modal';
 import Skeleton from 'react-loading-skeleton';
 import { copyElement } from '../../public/function';
+import NoConfigs from './NoConfigs';
 
 const Configs = () => {
     const [loading, setLoading] = useState(false)
     const [configs, setConfigs] = useState([]);
+    const [data, setData] = useState({all: true})
+    const [search, setSearch] = useState({})
     const allConfigs = async () => {
-        // setLoading(true)
         const configsList = await getConfigsList()
-        // if (configsList) setLoading(false)
-        setConfigs(configsList?.configs)
+        setConfigs(configsList.configs)
+    }
+    const configsByDate = async () => {
+        setLoading(true)
+        const configsList = await getConfigsByDay(search)
+        setLoading(false)
+        setConfigs(configsList.configs)
+    }
+    const click = (event) =>{
+        const name = event.target.getAttribute('name')
+        setData({[name]: true});
+        if(name == 'all') {
+            allConfigs()
+            setSearch({})
+        }
+    }
+    const change = (event) =>{
+        setSearch({[event.target.name]: event.target.value});
     }
     const changeStatus = async (userID, configID) => {
         setLoading(true)
@@ -23,9 +41,9 @@ const Configs = () => {
         if(response) setLoading(false)
         return response.configStatus
     }
-    
+                
     useEffect(() => {
-        allConfigs()
+        data.all && allConfigs()
         document.title = 'dashboard';
     }, []);
 
@@ -34,9 +52,22 @@ const Configs = () => {
             <Sidebar />
             <div className={styles.mainContainer} >
                 <div className={styles.container}>
-                    {/* <div className={`w-full top-0 relative h-[120px] border-2 p-1 mb-5 bg-white rounded-br-3xl rounded-bl-3xl  ${styles.filterHeader}`}>
-                        jjjjjjjjjjjjjjjj
-                    </div> */}
+                    <div className="flex bg-white justify-center px-3 py-1 border-2 m-7 rounded-full h-[50px]">
+                        <p className="flex justify-center items-center">فیلتر بر اساس:</p>
+                        <div className="flex items-center my-3">
+                            <span onClick={click} name='all' className={`px-3 py-2 mx-2 ${data.all? "rounded-md bg-blue-400 text-white": "hover:border-b-2 border-blue-400 transition duration-500 ease-out"}`}>همه</span>
+                            {/* <span onClick={click} name='percent' className={`px-3 py-2 mx-2 ${data.percent? "rounded-md bg-blue-400 text-white": "hover:border-b-2 border-blue-400 transition duration-500 ease-out"}`}>حجم باقی مانده</span> */}
+                            <span onClick={click} name='expiry' className={`px-3 py-2 mx-2 ${data.expiry? "rounded-md bg-blue-400 text-white": "hover:border-b-2 border-blue-400 transition duration-500 ease-out"}`}>زمان اتمام</span>
+                        </div>
+                        <div className="flex items-center my-3">
+                            {
+                                data.percent && <input value={search.percent} onChange={change} name='percent' type="text" className="px-2 text-sm outline-none py-2 mx-2 rounded-md border-2 border-gray-400 focus:border-blue-400" placeholder="حجم باقی مانده (درصد)" />
+                            }
+                            {
+                                data.expiry && <input value={search.day} onKeyUp={search.day? configsByDate: null} onChange={change} name='day' type="text" className="px-2 text-sm outline-none py-2 mx-2 rounded-md border-2 border-gray-400 focus:border-blue-400" placeholder="زمان باقی مانده (روز)" />
+                            }
+                        </div>
+                    </div>
                     <div className={styles.container}>
                     <div className="w-full h-[80px] border-2 m-1 rounded-full bg-white">
                         <div className="flex items-center h-full text-xl p-10" style={{direction: "ltr"}}>
@@ -47,9 +78,12 @@ const Configs = () => {
                             <div className="w-[15%] flex justify-center">جزیُیات</div>
                         </div>
                     </div>
-                        {   !configs.length?
-                            copyElement(<Skeleton className='w-[960px] h-[80px] border-2 m-2 rounded-full'/>, 4)
-                            :configs?.map(config => <Config key={config._id} config={config} changeStatus={changeStatus} setLoading={setLoading}/>)
+                        {   
+                            configs?.length?
+                            configs.map(config => <Config key={config._id} config={config} changeStatus={changeStatus} setLoading={setLoading}/>)
+                            :(
+                                configs == null? <NoConfigs /> : copyElement(<Skeleton className='w-[960px] h-[80px] border-2 m-2 rounded-full'/>, 4)
+                            )
                         }
                     </div>
                 </div>
---
+++ src/components/admin/config-section/NoConfigs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import emptyBox from '../assets/empty-box.png';
+import Add from '../assets/Add..svg';
+import { Link } from 'react-router-dom';
+
+const NoConfigs = () => {
+    return (
+        // <div className='h-[50vh] w-full'>
+            <div className='w-full h-[50vh] flex justify-center items-center'>
+                <div className='flex flex-col w-[40%]'>
+                    <div className='justify-center flex'>
+                        <img className="h-24" src={emptyBox} alt='empty box' />
+                    </div>
+                    <div className='flex justify-center mt-4'>
+                        <div className='w-fit text-sm font-bold'>
+                            <div className='flex justify-center'>هیچ کانفیگی ثبت نکردید</div>
+                            <Link to='/dashboard/plans'><button className='bg-blue-400 text-white flex p-2 rounded-md mt-5'>همین الان فروشت رو شروع کن<img className='pr-1 font-extrabold' src={Add}/></button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default NoConfigs;
