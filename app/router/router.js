aa01be1f1ce5a7570a37f54fd438dded6cf83b7d
matin dezhbani <dezhbanimatin@gmail.com>
2024-08-06 12:27:22 +0330

    create graphql routes

--- app/router/router.js
+++ app/router/router.js
@@ -1,15 +1,18 @@
+const { graphqlHTTP } = require("express-graphql");
 const { checkPermission } = require("../middlewares/permission.guard");
 const { verifyToken } = require("../middlewares/verifyAccessToken");
 const { AdminRoutes } = require("./admin/admin.routes");
 const { adminPaymentsRoutes } = require("./admin/payments");
-const { adminProfileRoutes } = require("./admin/profile");
+const { adminProfileRoutes } = require("./public/profile");
 const { userAuthRoutes } = require("./user/auth");
+const { graphqlConfig } = require("../graphql/configs/graphql.config");
 
 const router = require("express").Router(); 
 
 router.use("/auth", userAuthRoutes);
 router.use("/profile", verifyToken, adminProfileRoutes);
 router.use("/payment", adminPaymentsRoutes);
+router.use("/graphql", graphqlHTTP(graphqlConfig))
 router.use("/admin", verifyToken, AdminRoutes);
 
 module.exports = {
