f328cbeca978ae0591bfa0ed0f61eeb3cfe56570
matin dezhbani <dezhbanimatin@gmail.com>
2024-08-06 12:42:37 +0330

    update verify transaction

--- app/router/admin/payments.js
+++ app/router/admin/payments.js
@@ -3,7 +3,7 @@ const { verifyToken } = require("../../middlewares/verifyAccessToken");
 const router = require("express").Router();
 
 router.post("/create", paymentController.createTransaction);
-router.get("/verify/:billID/:authority", verifyToken, paymentController.verifyTransaction);
+router.post("/verify/:authority", verifyToken, paymentController.verifyTransaction);
 router.get("/verify/wallet/:billID/:authority", verifyToken, paymentController.verifyWalletTransaction);
 
 module.exports = {
