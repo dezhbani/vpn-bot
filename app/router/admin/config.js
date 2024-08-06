45d3cf4b83fa8c646568727d0761f3afed6e0a0a
matin dezhbani <dezhbanimatin@gmail.com>
2024-08-06 12:41:34 +0330

    create upgrade config plan routes

--- app/router/admin/config.js
+++ app/router/admin/config.js
@@ -7,6 +7,7 @@ router.get("/active/:userID", configController.getUserActiveConfigs);
 router.post("/endedTime", configController.getBYEndedTime);
 router.post("/changeStatus", configController.changeStatus);
 router.post("/add", configController.buyConfig);
+router.post("/upgrade", configController.upgradeConfig);
 router.delete("/delete", configController.deleteConfig);
 router.post("/repurchase", configController.repurchase)
 router.post("/replace", configController.replaceAllConfigs)
