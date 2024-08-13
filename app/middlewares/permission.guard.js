7d169341d9182c8bbc09b4e7bf96500d55cb2c7f
matin dezhbani <dezhbanimatin@gmail.com>
2024-08-13 16:27:08 +0330

    update check guard middleware

--- app/middlewares/permission.guard.js
+++ app/middlewares/permission.guard.js
@@ -5,28 +5,45 @@ const { mongoID } = require("../validations/public.schema");
 const { lastIndex } = require("../utils/functions");
 const { userModel } = require("../models/user");
 
-function checkPermission(){
+// function checkPermission(){
+//     return async function(req, res, next){
+//         try {
+//             const url = req._parsedUrl.path;
+//             const arrayOfUrl = url.split('/')
+//             const LastIndex = lastIndex(arrayOfUrl)
+//             const testID = mongoID.test(LastIndex);
+//             const index = arrayOfUrl.indexOf(LastIndex)
+//             if(testID) arrayOfUrl[index] = 'id';
+//             const { first_name, last_name } = req.user;
+//             const name = `${first_name} ${last_name}`
+//             let role 
+//             role = await roleModel.findOne({name});
+//             if(!role) {
+//                 const user = await userModel.findOne({first_name, last_name})
+//                 role = await roleModel.findOne({name: user.role})
+//             }
+//             // if(!role) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید')
+//             const permissions = await permissionModel.find({_id: {$in: role.permissions}});
+//             const permission = await permissionModel.findOne({main: arrayOfUrl[2], sub: arrayOfUrl[3]});
+//             const userPermission = permissions.filter(p => p?._id.toString() == permission?._id.toString());
+//             if (userPermission.length == 0) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید');
+//             return next()
+//         } catch (error) {
+//             next(error)
+//         }
+//     }
+// }
+function checkPermission(name){
     return async function(req, res, next){
         try {
-            const url = req._parsedUrl.path;
-            const arrayOfUrl = url.split('/')
-            const LastIndex = lastIndex(arrayOfUrl)
-            const testID = mongoID.test(LastIndex);
-            const index = arrayOfUrl.indexOf(LastIndex)
-            if(testID) arrayOfUrl[index] = 'id';
-            const { first_name, last_name } = req.user;
-            const name = `${first_name} ${last_name}`
-            let role 
-            role = await roleModel.findOne({name});
-            if(!role) {
-                const user = await userModel.findOne({first_name, last_name})
-                role = await roleModel.findOne({name: user.role})
-            }
-            // if(!role) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید')
-            const permissions = await permissionModel.find({_id: {$in: role.permissions}});
-            const permission = await permissionModel.findOne({main: arrayOfUrl[2], sub: arrayOfUrl[3]});
-            const userPermission = permissions.filter(p => p?._id.toString() == permission?._id.toString());
-            if (userPermission.length == 0) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید');
+            const { first_name, last_name, role } = req.user;
+            const userFullName = `${first_name} ${last_name}`
+            let userRole = await roleModel.findOne({userFullName})
+            if(!userRole) userRole = await roleModel.findOne({name: role})
+            if(!userRole) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید')
+            const permissions = await permissionModel.find({_id: {$in: userRole.permissions}});
+            const checkUserAccess = permissions.find(permission => permission.name == name)
+            if(!checkUserAccess) throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید')
             return next()
         } catch (error) {
             next(error)
