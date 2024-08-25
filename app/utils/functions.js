c6a1afc1702cf24aa0e20c5459fcb51d9c0db6bf
matin dezhbani <dezhbanimatin@gmail.com>
2024-08-25 12:16:48 +0330

    fix delete key bug

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -45,14 +45,15 @@ const randomString = () => {
     return random
 }
 function deleteInvalidProperties(data = {}, blackListFields){
-    let nullishData = ["", " ", "0", 0, null, undefined];
+    let nullishData = ["", " ", "0", 0, null, undefined]
     Object.keys(data).forEach(key => {
-        if(blackListFields.includes(data[key])) delete data[key];
-        if(typeof data[key] === "string") data[key] = data[key].trim();
-        if(Array.isArray(data[key]) && data[key].length > 0) data[key] = data[key].map(item => item.trim());
-        if(Array.isArray(data[key]) && data[key].length == 0) delete data[key];
-        if(nullishData.includes(data[key])) delete data[key];
+        if (blackListFields.includes(key)) delete data[key]
+        if (typeof data[key] == "string") data[key] = data[key].trim();
+        if (Array.isArray(data[key]) && data[key].length > 0) data[key] = data[key].map(item => item.trim())
+        if (Array.isArray(data[key]) && data[key].length == 0) delete data[key]
+        if (nullishData.includes(data[key])) delete data[key];
     })
+    
 }
 function copyObject(object){
     return JSON.parse(JSON.stringify(object))
