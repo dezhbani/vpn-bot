644d42495c7e4441a3020ecbe605bab4fb68d807
matin dezhbani <dezhbanimatin@gmail.com>
2024-10-09 22:46:31 +0330

    add loading to Profile context to when request to api show loading

--- src/components/context/UserProfileContext.js
+++ src/components/context/UserProfileContext.js
@@ -1,19 +1,23 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { getUserProfile } from '../admin/services/profile.service';
+import Modal from '../public/components/Modal';
 
 export const ProfileContext = createContext()
 
 const UserProfileContext = ({children}) => {
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
     const UserProfile = async () => {
         const result = await getUserProfile()
         setProfile(result)
+        setLoading(false)
     }
     useEffect(() => {
         UserProfile()
     }, [])
     return (
         <div>
+            <Modal isOpen={loading} loading={loading} />
             <ProfileContext.Provider value={profile}>
                 {children}
             </ProfileContext.Provider>
