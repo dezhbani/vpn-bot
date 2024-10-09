a9702188005e672783ead28ceff7f8f200e7cd97
matin dezhbani <dezhbanimatin@gmail.com>
2024-10-09 21:44:13 +0330

    set modal z-index

--- src/components/public/components/Modal.js
+++ src/components/public/components/Modal.js
@@ -1,39 +1,28 @@
-// Modal.js
-
 import React from 'react';
-import './Modal.css'
 import Loading from './Loading';
 
-const Modal = ({ isOpen , onClose, children, loading }) => {
-
-  document.body.style.overflow = isOpen? 'hidden' : ''
+const Modal = ({ isOpen, onClose, children, loading, wFitContent = false }) => {
+  // Prevent scrolling when the modal is open
+  document.body.style.overflow = isOpen ? 'hidden' : '';
 
   return (
-    isOpen && loading? (
-      <div className="modal-overlay z-50">
-        <div className='bg-white opacity-50 h-screen w-screen fixed'></div>
-        <div className='modal-container'>
-                <Loading />
+    isOpen && loading ? (
+      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-80"> {/* Higher z-9ndex */}
+        <div className={`bg-white opacity-50 fixed  z-80 ${wFitContent ? 'w-fit h-fit' : 'h-screen w-screen'}`}></div>
+        <div className="absolute z-90">
+          <Loading />
         </div>
       </div>
-    ): isOpen && (
-      <div className="modal-overlay">
-          <div onClick={onClose} className='bg-black opacity-50 h-full w-full fixed'></div>
-          <div className='modal-container'>
-              <div className="modal">
-                  <div className="modal-content">{children}</div>
-              </div>
-          </div>
+    ) : isOpen && (
+      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-80"> {/* Higher z-9ndex */}
+        <div onClick={onClose} className="bg-black opacity-50 h-full w-full fixed z-80"></div>
+        <div className="bg-white rounded-md shadow-lg max-w-full max-h-[95%] overflow-auto z-90">
+          <div className='flex'>{children}</div>
+        </div>
       </div>
     )
   );
 };
 
-// Modal.propTypes = {
-//   isOpen: PropTypes.bool.isRequired,
-//   onClose: PropTypes.func.isRequired,
-//   children: PropTypes.node.isRequired,
-//   children: PropTypes.node.isRequired,
-// };
-
 export default Modal;
+
