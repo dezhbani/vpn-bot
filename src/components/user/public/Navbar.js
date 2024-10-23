76ac9a5bbbea828ae534b894bbc207c85d0f3fa6
matin dezhbani <dezhbanimatin@gmail.com>
2024-10-23 12:12:14 +0330

    add link for move to profile section & when click on outside close Profile

--- src/components/user/public/Navbar.js
+++ src/components/user/public/Navbar.js
@@ -1,42 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import Notification from '../assets/Notification.svg';
 import ProfileIcon from '../assets/Profile.svg';
 import Profile from './Profile';
 
 const Navbar = () => {
     const [openProfile, setOpenProfile] = useState(false);
-    const profileRef = useRef(null);
 
     const handleOpenProfile = () => setOpenProfile(!openProfile);
 
-    // Close dropdown when clicking outside
-    const handleClickOutside = (event) => {
-        if (profileRef.current && !profileRef.current.contains(event.target)) {
-            setOpenProfile(false);
-        }
-    };
-
-    useEffect(() => {
-        // Add event listener to detect clicks outside
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            // Cleanup the event listener when component unmounts
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
     return (
-        <div className='bg-[#fff]/50 w-full pt-8 z-40 backdrop-blur-sm fixed top-0'>
-            <div className='flex bg-white w-4/5 shadow-[2px_4px_30px_0px_#00000010] mx-5 rounded-xl'>
-                <div className='p-2 m-2' ref={profileRef} onClick={handleOpenProfile}>
-                    <img className='h-8' src={ProfileIcon} alt='Profile' />
-                </div>
-                    {openProfile && <Profile />}
-                <div className='p-2 m-2'>
-                    <img className='h-8' src={Notification} alt='Notification' />
+        // <div className='h-full w-full pt-8 z-40 fixed top-0'>
+            <div className='bg-[#fff]/50 w-full pt-8 z-40 backdrop-blur-sm fixed top-0'>
+                <div className='flex bg-white w-4/5 shadow-[2px_4px_30px_0px_#00000010] mx-5 rounded-xl'>
+                    <div className='p-2 m-2' onClick={handleOpenProfile}>
+                        <img className='h-8' src={ProfileIcon} alt='Profile' />
+                    </div>
+                    {openProfile && <Profile setOpenProfile={setOpenProfile} />}
+                    <div className='p-2 m-2'>
+                        <img className='h-8' src={Notification} alt='Notification' />
+                    </div>
                 </div>
             </div>
-        </div>
+        // </div>
     );
 };
 
--- src/components/user/public/Profile.js
+++ src/components/user/public/Profile.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ExitIcon from '../assets/Exit.svg';
 import SettingIcon from '../assets/Setting.svg';
 import ProfileIcon from '../assets/Profile-2.svg';
 import Modal from '../../public/components/Modal';
+import { Link } from 'react-router-dom';
 
 const ConfirmExitAccount = ({status, close}) => {
     const exitAccount = () => {
@@ -11,7 +12,7 @@ const ConfirmExitAccount = ({status, close}) => {
     }
     return (
         <Modal isOpen={status}>
-            <div className='p-10 font-b-kamran text-2xl'>
+            <div className='p-20 font-b-kamran text-2xl'>
                 <p className='text-3xl'>آیا می‌خواهید از حساب‌ کاربری خود خارج شوید؟</p>
                 <div className='mt-8 w-full  flex items-center'>
                     <button onClick={exitAccount} className='bg-red-500 flex items-center justify-center rounded-md py-1 mx-3 text-white w-1/2'>خروج</button>
@@ -22,23 +23,46 @@ const ConfirmExitAccount = ({status, close}) => {
     )
 }
 
-const Profile = () => {
+const Profile = ({setOpenProfile}) => {
     const [openConfirm, setOpenConfirm] = useState(false)
+    const profileRef = useRef(null);
+
+
+    // Close dropdown when clicking outside
+    const handleClickOutside = (event) => {
+        if (profileRef.current && !profileRef.current.contains(event.target)) {
+            setOpenProfile(false);
+        }
+    };
+
+    useEffect(() => {
+        // Add event listener to detect clicks outside
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            // Cleanup the event listener when component unmounts
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     const handleOpenConfirm = () => {
         setOpenConfirm(true)
     }
     const handleCloseConfirm = () => setOpenConfirm(false)
 
     return (
-        <>
+        <div ref={profileRef} >
             <ConfirmExitAccount close={handleCloseConfirm} status={openConfirm} />
-            <div className='bg-white flex flex-col absolute w-48 mt-20 py-4 px-6 rounded-xl shadow-[2px_4px_30px_0px_#00000010] dir-rtl font-b-kamran text-2xl'>
-                <div className='py-1 flex items-center'><img className='h-6' src={ProfileIcon}/><span className='mx-2'>پروفایل</span></div>
+            <div className='bg-white -mx-16 flex flex-col absolute w-48 mt-20 py-4 px-6 rounded-xl shadow-[2px_4px_30px_0px_#00000010] dir-rtl font-b-kamran text-2xl z-10'>
+                <Link to='/profile'>
+                    <div className='flex items-center'>
+                        <img className='h-6' src={ProfileIcon}/>
+                        <span className='mx-2'>پروفایل</span>
+                    </div>
+                </Link>
                 <div className='py-1 flex items-center'><img className='h-6' src={SettingIcon}/><span className='mx-2'>تنظیمات</span></div>
                 <div className='border-main-blue opacity-60 border-t-2 my-2' />
                 <div className='py-1 flex items-center' onClick={handleOpenConfirm}><img className='h-6' src={ExitIcon}/><span className='mx-2'>خروج</span></div>
             </div>
-        </>
+        </div>
     );
 };
 
