7c7674e8562e8173b253ff3f8131626ba00cc6e2
matin dezhbani <dezhbanimatin@gmail.com>
2024-11-01 16:26:19 +0330

    create config and plan api routes

--- app/router/user/user.routes.js
+++ app/router/user/user.routes.js
@@ -1,8 +1,12 @@
+const { userConfigRoutes } = require("./config");
+const { userPlanRoutes } = require("./plan");
 const { userProfileRoutes } = require("./profile");
 
 const router = require("express").Router();
 
 router.use("/profile", userProfileRoutes);
+router.use("/config", userConfigRoutes);
+router.use("/plan", userPlanRoutes);
 
 module.exports = {
     UserRoutes: router
