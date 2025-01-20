442c2f2aca04e4d2b2f24fad3cfde977b17d17f0
matin dezhbani <dezhbanimatin@gmail.com>
2025-01-20 22:01:39 +0330

    change database

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const Application = require("./app/server");
-// new Application(80, "mongodb://admin:yourpassword@87.248.150.99:27000/admin?directConnection=true")
-new Application(80, "mongodb://127.0.0.1:27017/vpn-store")
+new Application(80, "mongodb://admin:yourpassword@87.248.150.99:27000/admin?directConnection=true")
+// new Application(80, "mongodb://127.0.0.1:27017/vpn-store")
