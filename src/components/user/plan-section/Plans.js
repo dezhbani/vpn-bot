fce956591d112f3ba815481834c7b63aa02d01d1
matin dezhbani <dezhbanimatin@gmail.com>
2025-02-05 12:08:24 +0330

    feat: add loading to plan section

--- src/components/user/plan-section/Plans.js
+++ src/components/user/plan-section/Plans.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { getPlans } from '../services/plan.service';
 import Sidebar from '../public/Sidebar';
 import Plan from './Plan';
+import Modal from '../../public/components/Modal';
 
 const Plans = () => {
-    const [plans, setPlans] = useState([])
+    const [plans, setPlans] = useState(null)
 
     const getPlanList = async () => {
         const result = await getPlans()
@@ -19,9 +20,11 @@ const Plans = () => {
         <>
             <Sidebar />
             {/* <Navbar /> */}
-            <div className='flex z-20 dir-rtl w-[calc(100%-7rem)] sm:w-[calc(75%-3.5rem)] lg:w-[calc(80%-2rem)] xl:w-4/5 mt-28 flex-wrap'>
+            <div className='flex z-20 dir-rtl w-[calc(100%-5rem)] sm:w-[calc(75%-3.5rem)] lg:w-[calc(80%-2rem)] xl:w-4/5 mt-28 max-md:justify-center flex-wrap'>
                 {
-                    plans?.map(plan => <Plan key={plan._id} plan={plan} />)
+                    plans?
+                    plans?.map(plan => <Plan key={plan._id} plan={plan} />):
+                    <Modal isOpen={true} loading={true} />
                 }
             </div>
         </>
